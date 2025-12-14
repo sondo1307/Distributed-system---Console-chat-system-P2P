bf5cab9794954923f905785d5b059b759ce2a818
sondo1307 <56852370+sondo1307@users.noreply.github.com>
2025-12-14 23:11:48 +0700

    4

--- P2P - Chat System C#/P2P Chat Console/obj/Debug/net10.0/P2P Chat Console.AssemblyInfo.cs
+++ P2P - Chat System C#/P2P Chat Console/obj/Debug/net10.0/P2P Chat Console.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2024")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Ung dung chat noi bo an toan")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7853c8f62e128d63514109a6477bf2bd31950e49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66393dc90aa6c77f11191f167c4887df812d0ead")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2P Chat System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2P Chat Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
